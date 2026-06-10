e2ec5bb7ee23271a4491d870c886d03e7e7df38e
Allester Padovani <63115534+IntuneAdministrator@users.noreply.github.com>
2026-06-10 14:30:37 -0500

    Delete Desktop Shortcut directory

--- Desktop Shortcut/check.ps1
+++
@@ -1,8 +0,0 @@
-$PackageName = "DesktopIcon"
-$Version = "1"
-$Path_local = "$Env:Programfiles\MEM"
-$ProgramVersion_current = Get-Content -Path "$Path_local\Validation\$PackageName"
-
-if($ProgramVersion_current -eq $Version){
-    Write-Host "Found it!"
-}
